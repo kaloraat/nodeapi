476c66af27d97cca0005a1587a5d2583603dc58c
Ryan Dhungel <kaloraat@gmail.com>
2019-01-16 20:13:54 +1100

    31

--- controllers/post.js
+++ controllers/post.js
@@ -1,15 +1,18 @@
 const Post = require("../models/post");
 
 exports.getPosts = (req, res) => {
-    res.json({
-        posts: [{ title: "First post" }, { title: "Second post" }]
-    });
+    const posts = Post.find()
+        .select("_id title body")
+        .then(posts => {
+            res.json({ posts });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.createPost = (req, res) => {
     const post = new Post(req.body);
     post.save().then(result => {
-        res.status(200).json({
+        res.json({
             post: result
         });
     });
--- routes/post.js
+++ routes/post.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const postController = require("../controllers/post");
+const { getPosts, createPost } = require("../controllers/post");
 const validator = require("../validator");
 
 const router = express.Router();
 
-router.get("/", postController.getPosts);
-router.post("/post", validator.createPostValidator, postController.createPost);
+router.get("/", getPosts);
+router.post("/post", validator.createPostValidator, createPost);
 
 module.exports = router;
