6e07922df74eca705cf9d8ccc359d0856f762359
Ryan Dhungel <kaloraat@gmail.com>
2019-02-01 23:17:03 +1100

    return all posts in array format FIXED

--- controllers/post.js
+++ controllers/post.js
@@ -22,7 +22,7 @@ exports.getPosts = (req, res) => {
         .populate("postedBy", "_id name")
         .select("_id title body")
         .then(posts => {
-            res.json({ posts });
+            res.json(posts);
         })
         .catch(err => console.log(err));
 };
