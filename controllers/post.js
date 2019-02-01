5f3694700359de6fbb4003797055887b8d5ad1d2
Ryan Dhungel <kaloraat@gmail.com>
2019-02-02 00:41:19 +1100

    getPosts updated with sorting and date

--- controllers/post.js
+++ controllers/post.js
@@ -20,7 +20,8 @@ exports.postById = (req, res, next, id) => {
 exports.getPosts = (req, res) => {
     const posts = Post.find()
         .populate("postedBy", "_id name")
-        .select("_id title body")
+        .select("_id title body created")
+        .sort({ created: -1 })
         .then(posts => {
             res.json(posts);
         })
